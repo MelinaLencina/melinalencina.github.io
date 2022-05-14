f6379368f46a034e3e9d0380b144050fc4f1fa45
Melina Lencina <melina00lencina@gmail.com>
2022-05-13 23:32:35 -0300

    2nd exercise JS

--- CILSA/js/act2/nuevocodigo.js
+++ CILSA/js/act2/nuevocodigo.js
@@ -1,4 +1,10 @@
+//La siguiente línea imprime por consola un saludo
+
 console.log("¡Hola, mundo! Este es mi primer script");
+
+
+/*Se detalla a continuación cómo insertar código JavaScript en un documento HTML,
+utilizamos document.write() para insertar texto en HTML desde JS */
 
 document.write(`La primera forma de insertar JavaScript en HTML es directa. <br>
 Podés hacerlo utilizando la etiqueta script que debe envolver<br>
@@ -12,5 +18,8 @@ Se puede hacer referencia a estos archivos desde adentro de los documentos HTML,
 al igual que hacemos con los estilos de CSS; en el caso de JavaScript, <br>
 se utiliza la etiqueta script con el atributo src (source = fuente), que tendrá<br>
 como valor (entre comillas) el nombre de nuestro archivo externo .js.`);
+
+
+//Esta alerta nos muestra un breve mensaje
 
 alert(`Ver lo simple que es agregar "comillas dobles" y ‘comillas simples’`);
