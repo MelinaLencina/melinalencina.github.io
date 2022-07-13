20d93ece781ee4da9b0b6f27b81b73e05e41e020
Melina Lencina <melina00lencina@gmail.com>
2022-07-13 16:04:45 -0300

    Update: comments were added

--- CILSA/js/reloj/assets/js/script.js
+++ CILSA/js/reloj/assets/js/script.js
@@ -1,5 +1,8 @@
 (function(){
     var refreshTime = function(){
+
+// Toma los valores para generar la fecha
+
         var date = new Date(),
             hours = date.getHours(),
             amPm,
@@ -10,6 +13,8 @@
             month = date.getMonth(),
             year = date.getFullYear();
 
+// Generamos las variables de cada dato a mostrar en el reloj
+
         var pHour = document.getElementById('hours'),
         pAmPm = document.getElementById('amPm'),
         pMinutes = document.getElementById('minutes'),
@@ -18,6 +23,8 @@
         pDay = document.getElementById('day'),
         pMonth = document.getElementById('month'),
         pYear = document.getElementById('year');
+
+// Creamos el array con todos los días de la semana y luego el array con los meses del año
 
         var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         pDayWeek.textContent = week[dayWeek];
@@ -29,6 +36,8 @@
 
         pYear.textContent = year;
 
+// Generamos un condicional que mantiene las horas en formato 12 HRS para el uso de AM/PM
+
         if (hours >= 12) {
             hours = hours - 12;
             amPm = 'PM';
@@ -36,13 +45,17 @@
             amPm = 'AM';
         }
 
-        if (hours < 10){
-            hours = "0" + hours;
-        }
+// Creamos un condicional que iguala la hora 00 a 12, respetando el formato 12 HRS
 
         if (hours == 0) {
             hours = 12
         };
+
+// Generamos un condicional que le agrega un 0 a la izquierda a las horas, minutos y segundos, respectivamente, si éstas son de 1 sólo dígito, manteniendo el formato esperado
+
+        if (hours < 10){
+            hours = "0" + hours;
+        }
 
         pHour.textContent = hours;
         pAmPm.textContent = amPm;
@@ -61,10 +74,13 @@
 
     };
 
+// La siguiente variable hace que corran los segundos en el reloj
+
     refreshTime();
     var interval = setInterval(refreshTime, 1000);
 }())
 
+// El siguiente evento se encarga de la función de light/dark mode, mediante un click se realiza el cambio de un modo a otro
 
 const buttonSwitch = document.querySelector('#switch');
 
